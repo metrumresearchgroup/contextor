31ce6eb95282f6ce83d95eabdffc525b92284d0d
Elijah Sarver <elis@metrumrg.com>
2024-04-25 13:20:18 -0500

    Update to drop the interface.

--- contextor.go
+++ contextor.go
@@ -8,36 +8,30 @@ import (
 
 var ErrWrongType = errors.New("wrong type")
 
-// Contextor is the general interface to the context.
-type Contextor[T any] interface {
-	Get(ctx context.Context) (T, error)
-	Set(context.Context, T) (context.Context, error)
-}
-
 // New creates a new Contextor for any type, using the label indicated
 // to make it unique. (if necessary)
 //
 // You can leave the label empty if you only expect one copy of a particular
 // type in the system.
-func New[T any](label string) Contextor[T] {
+func New[T any](label string) Contextor[T, ctxKey] {
 	var empty T
 
-	return &contextor[T, ctxKey]{key: ctxKey(fmt.Sprintf("%T-%s", empty, label))}
+	return Contextor[T, ctxKey]{key: ctxKey(fmt.Sprintf("%T-%s", empty, label))}
 }
 
-func NewProvidedKey[T any, K comparable](providedKey K) Contextor[T] {
-	return &contextor[T, K]{key: providedKey}
+func NewProvidedKey[T any, K comparable](providedKey K) Contextor[T, K] {
+	return Contextor[T, K]{key: providedKey}
 }
 
 // Set places a value on the context, creating a key from the label and
 // type.
-func (c *contextor[T, K]) Set(ctx context.Context, v T) (context.Context, error) {
+func (c Contextor[T, K]) Set(ctx context.Context, v T) (context.Context, error) {
 	return context.WithValue(ctx, c.key, v), nil
 }
 
 // Get retrieves a value from context, creating the key on the fly
 // to match the label and type.
-func (c *contextor[T, K]) Get(ctx context.Context) (T, error) {
+func (c Contextor[T, K]) Get(ctx context.Context) (T, error) {
 	var empty T
 	var v T
 	a := ctx.Value(c.key)
@@ -54,5 +48,5 @@ func (c *contextor[T, K]) Get(ctx context.Context) (T, error) {
 
 type (
 	ctxKey                         string
-	contextor[T any, K comparable] struct{ key K }
+	Contextor[T any, K comparable] struct{ key K }
 )
