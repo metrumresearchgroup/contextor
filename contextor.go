ce1b12495057fc1e75ac6fba44d456ecc1d15cf2
Eli Sarver <eli.sarver@gmail.com>
2024-04-25 13:56:05 -0500

    Simplification of design (#2)
    * drop the interface.
    * clean up ErrWrongType to contain context of error.
    * drop key suffix of "-" when label is blank.
    
    ---------
    
    Co-authored-by: Elijah Sarver <elis@metrumrg.com>

--- contextor.go
+++ contextor.go
@@ -2,57 +2,59 @@ package contextor
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"strings"
 )
 
-var ErrWrongType = errors.New("wrong type")
-
-// Contextor is the general interface to the context.
-type Contextor[T any] interface {
-	Get(ctx context.Context) (T, error)
-	Set(context.Context, T) (context.Context, error)
-}
+type (
+	Contextor[T any, K comparable] struct {
+		key K
+	}
+	ctxKey string
+)
 
 // New creates a new Contextor for any type, using the label indicated
 // to make it unique. (if necessary)
 //
 // You can leave the label empty if you only expect one copy of a particular
 // type in the system.
-func New[T any](label string) Contextor[T] {
-	var empty T
+func New[T any](label string) Contextor[T, ctxKey] {
+	vals := []string{fmt.Sprintf("%T", *new(T))}
+	if len(label) > 0 {
+		vals = append(vals, label)
+	}
+	key := strings.Join(vals, "-")
 
-	return &contextor[T, ctxKey]{key: ctxKey(fmt.Sprintf("%T-%s", empty, label))}
+	return Contextor[T, ctxKey]{key: ctxKey(key)}
 }
 
-func NewProvidedKey[T any, K comparable](providedKey K) Contextor[T] {
-	return &contextor[T, K]{key: providedKey}
+func NewProvidedKey[T any, K comparable](providedKey K) Contextor[T, K] {
+	return Contextor[T, K]{key: providedKey}
 }
 
 // Set places a value on the context, creating a key from the label and
 // type.
-func (c *contextor[T, K]) Set(ctx context.Context, v T) (context.Context, error) {
+func (c Contextor[T, K]) Set(ctx context.Context, v T) (context.Context, error) {
 	return context.WithValue(ctx, c.key, v), nil
 }
 
 // Get retrieves a value from context, creating the key on the fly
 // to match the label and type.
-func (c *contextor[T, K]) Get(ctx context.Context) (T, error) {
-	var empty T
-	var v T
-	a := ctx.Value(c.key)
-	v, ok := a.(T)
-
-	if !ok {
-		return empty, fmt.Errorf("target type %T expected; value from context was type %T: %w", v, a, ErrWrongType)
+func (c Contextor[T, K]) Get(ctx context.Context) (T, error) {
+	raw := ctx.Value(c.key)
+	value, ok := raw.(T)
+	if ok {
+		return value, nil
 	}
 
-	return v, nil
+	return *new(T), &ErrWrongType[T]{actual: raw, wanted: value}
 }
 
-// inner types
+type ErrWrongType[T any] struct {
+	actual any
+	wanted T
+}
 
-type (
-	ctxKey                         string
-	contextor[T any, K comparable] struct{ key K }
-)
+func (e ErrWrongType[T]) Error() string {
+	return fmt.Sprintf("wrong type for key: %T; wanted: %T", e.actual, e.wanted)
+}
